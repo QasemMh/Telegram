77e78f17063b82148f4d1399caed639b3a7ee0a6
Qasem Mohammed <56113217+QasemMh@users.noreply.github.com>
2022-09-04 03:36:18 +0300

    add chat ui

--- src/app/user/chat/chat.component.ts
+++ src/app/user/chat/chat.component.ts
@@ -1,27 +1,41 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, Input } from '@angular/core';
 import { SignalrService } from 'src/app/signalr.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
 })
-export class ChatComponent implements OnInit ,OnDestroy{
+export class ChatComponent implements OnInit, OnDestroy {
+  @Input() userChat?: any;
 
-  constructor( public signalr:SignalrService) { }
-  
+  constructor(public signalr: SignalrService) {}
 
-  ngOnInit(): void {
-    this.signalr.startConnection();
-setTimeout(() => {
-  this.signalr.askServerListener();
-  this.signalr.askServer();
-  
-}, 2000);
+  ngOnInit(): void {}
+
+  ngOnDestroy() {}
+
+  sendMessage(event: any) {
+    const files = !event.files
+      ? []
+      : event.files.map((file: any) => {
+          return {
+            url: file.src,
+            type: file.type,
+            icon: 'file-text-outline',
+          };
+        });
+
+    this.userChat.userMessages.push({
+      text: event.message,
+      date: new Date(),
+      reply: true,
+      type: files.length ? 'file' : 'text',
+      files: files,
+      user: {
+        name: this.userChat.name,
+        avatar: 'https://i.gifer.com/no.gif',
+      },
+    });
   }
-
-  ngOnDestroy() {
-    this.signalr.hubConnrction?.off("askServerResponse")
-  }
-  
 }
