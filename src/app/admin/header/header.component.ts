ab51b0677f4025e83ad261e5058145d5c9cfbb78
DeveloperOsamaJaradat‏ <osamajaradat198@gmail.com>
2022-09-11 23:25:03 +0300

    post and report

--- src/app/admin/header/header.component.ts
+++ src/app/admin/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { NbSidebarService } from '@nebular/theme';
 
 @Component({
@@ -9,11 +10,15 @@ import { NbSidebarService } from '@nebular/theme';
 export class HeaderComponent implements OnInit {
 
 
-  constructor(private readonly sidebarService: NbSidebarService) { }
+  constructor(private readonly sidebarService: NbSidebarService ,private router:Router) { }
   toggleSidebar(): boolean {
     this.sidebarService.toggle();
     return false;
 
+  }
+  logout(){
+    localStorage.clear();
+    this.router.navigate(['/auth/login']);
   }
   ngOnInit(): void {
   }
--- src/app/admin/post/post.component.ts
+++ src/app/admin/post/post.component.ts
@@ -13,26 +13,235 @@ import { AdminService } from 'src/app/services/admin.service';
 })
 export class PostComponent implements OnInit {
   AllPost:any=[{}];
+  CommentByPostID:any=[{}];
+  commentsList:any=[{}];
+  ReactionList :any =[{}];
+  message :any;
+  isLike :boolean;
+  currentuserId:any;
+
+
+
   constructor(private router:Router , public home:HomeService ,private http:HttpClient ,private spinner :NgxSpinnerService ,private toster:ToastrService ,public admin:AdminService) { }
 
   ngOnInit(): void {
     //  this.admin.AllPost();
 
-     this.GetAllPost()
+     this.GetAllPost();
+     this.GetAllComments()
+     this.GetAllReaction()
+     let User : any = localStorage.getItem("userData");
+     User =JSON.parse(User);
+     this.currentuserId=User.userid;
+    console.log("this.userId",this.currentuserId)
   }
 
+
+
   GetAllPost(){
-    this.spinner.show();
-     this.http.get('https://localhost:44301/api/Post/GetAllpost').subscribe((res)=>{
+
+     this.http.get('https://localhost:44301/api/Post/AllPost').subscribe((res)=>{
+      // this.spinner.show();
       this.AllPost=res;
-      this.spinner.hide();
-      this.toster.success('Data Retriveed !' );
+      // this.spinner.hide();
+      // this.toster.success('Data Retriveed !' );
+
     },err=>{
-    this.spinner.hide();
+    // this.spinner.hide();
       this.toster.error(err.message,err.status );
     }
     )
 
   }
+
+  UpdatePost(id:number){
+
+  }
+
+
+
+  DeletePost(id:number){
+    console.log("id" , id)
+    this.http.delete('https://localhost:44301/api/Post/DeletePost/delete/' +id)
+
+      .subscribe(
+        (resp) => {
+          console.log("id 2" , id);
+        this.toster.success('Delete Post !' );
+        },
+        (err) => {}
+      );
+       window.location.reload();
+  }
+
+
+
+  // GetAllComments(){
+  //   this.http.get('https://localhost:44301/api/Comments/GetAllComments').subscribe((res)=>{
+
+  //       this.commentsList=res;
+  //       console.log("commentsList",this.commentsList);
+
+  //       //this.spinner.hide();
+  //       //this.toster.success('Data Retriveed !' );
+
+  //     },err=>{
+  //     this.spinner.hide();
+  //       this.toster.error(err.message,err.status );
+  //     }
+  //     )
+  // }
+
+GetAllComments(){
+  this.http.get('https://localhost:44301/api/Comments/GetAllCommentPost').subscribe((res)=>{
+
+      this.commentsList=res;
+      console.log("commentsList",this.commentsList);
+
+      //this.spinner.hide();
+      //this.toster.success('Data Retriveed !' );
+
+    },err=>{
+    this.spinner.hide();
+      this.toster.error(err.message,err.status );
+    }
+    )
+}
+
+
+  GetAllReaction(){
+    this.http.get('https://localhost:44301/api/Reaction/getAllReaction').subscribe((resp:any)=>{
+      this.ReactionList=resp;
+      console.log("ReactionList",this.ReactionList);
+
+
+    },
+      err=>{
+
+        this.toster.error(err.message , err.status)
+      }
+    )
+  }
+
+
+
+  AddComment(obj:any){
+   let User : any = localStorage.getItem("userData");
+   User =JSON.parse(User);
+   let uid : number =  +User.userid;
+    let addCom ={
+      UserId : uid,
+      PostId :obj.id,
+      Content :this.message,
+    }
+    console.log(obj);
+    console.log(addCom);
+    this.http.post('https://localhost:44301/api/Comments/InsertComment' ,addCom).subscribe((resp:any)=>{
+      // console.log(resp);
+      // this.spinner.hide();
+    this.toster.success(' send comment Successfully :)')
+    },
+      err=>{
+        this.spinner.hide()
+         this.toster.error(err.message , err.status)
+      }
+    )
+  }
+
+  GetCommentByPostID(Postid:number){
+    this.http.get('https://localhost:44301/api/Comments/GetPostComments/'+ Postid).subscribe((resp:any)=>{
+      this.CommentByPostID=resp;
+      // this.spinner.hide();
+       this.toster.success('Successfully :)')
+    },
+      err=>{
+        // this.spinner.hide()
+        this.toster.error(err.message , err.status)
+      }
+    )
+  }
+
+
+  CheckLike(postid :number , userid :number){
+    this.http.get('https://localhost:44301/api/Comments/GetPostComments/'+ userid +"/" + postid).subscribe((resp:any)=>{
+      this.isLike=resp;
+    // this.spinner.hide();
+     this.toster.success('Successfully :)')
+     console.log(this.isLike)
+
+  },
+    err=>{
+      // this.spinner.hide()
+      this.toster.error(err.message , err.status)
+    }
+  )
+  }
+
+  DeleteLike(obj){
+    let User : any = localStorage.getItem("userData");
+    User =JSON.parse(User);
+    let uid : number =  +User.userid;
+     let DeleteLike ={
+      userId : uid,
+      postId :obj.id,
+
+
+     }
+
+
+     this.http.delete('https://localhost:44301/api/Reaction/DeleteReaction/'+DeleteLike.userId +'/'+DeleteLike.postId)
+
+     .subscribe(
+       (resp) => {
+
+       this.toster.success('Delete Like !' );
+       },
+       (err) => {}
+     );
+
+  }
+
+
+
+  createLike(obj){
+    let User : any = localStorage.getItem("userData");
+    User =JSON.parse(User);
+    let uid : number =  +User.userid;
+     let addLike ={
+      user_id : uid,
+      post_id :obj.id,
+      is_react :1,
+
+     }
+
+     if(this.currentuserId==addLike.user_id)
+
+    this.http.post('https://localhost:44301/api/Reaction/InsertReaction',addLike).subscribe((resp:any)=>{
+      console.log(resp);
+      // this.spinner.hide();
+    //this.toster.success(' send comment Successfully :)')
+    this.GetAllReaction()
+    },
+      err=>{
+        this.spinner.hide()
+         this.toster.error(err.message , err.status)
+      }
+    )
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
