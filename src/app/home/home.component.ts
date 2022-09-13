d1d12400822f09535b0a98ae8a2b49273e5923bc
Qasem Mohammed <56113217+QasemMh@users.noreply.github.com>
2022-09-13 21:18:02 +0300

    fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,5 @@
+import { NgxSpinnerService } from 'ngx-spinner';
+import { UserService } from 'src/app/services/user.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { HomeService } from '../services/home.service';
@@ -5,63 +7,52 @@ import { HomeService } from '../services/home.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-// //AboutUsInfo
-//   @Input()id :number|undefined
-//   @Input()img :string|undefined
-//   @Input()contant :string|undefined
-
-  // //HomeInfo
-  // @Input()name :string|undefined
-  // @Input()logo :string|undefined
-  // @Input()email :string|undefined
-  // @Input()phone :string|undefined
-  // @Input()address :string|undefined
-
-CreateContact:FormGroup=new FormGroup({
-  username :new FormControl(),
-  email  :new FormControl(),
-  subject  :new FormControl(),
-  message  :new FormControl()
-})
-  constructor(public home :HomeService) { }
-
+  CreateContact: FormGroup = new FormGroup({
+    username: new FormControl(),
+    email: new FormControl(),
+    subject: new FormControl(),
+    message: new FormControl(),
+  });
+  constructor(
+    public home: HomeService,
+    private userService: UserService,
+    private spinner: NgxSpinnerService
+  ) {}
+  IsLogin: boolean = false;
+  userData: any;
+  userPath: any = '/user';
   ngOnInit(): void {
-
+    this.spinner.show();
     this.home.getHomeInfo();
     this.home.getAboutUsInfo();
     this.home.GetAcceptTestimonial();
     this.home.GetTop5Like();
     this.home.GetTop5Comment();
-    console.log(this.home.HomePage.address)
-   // console.log(this.home.getAboutUsInfo())
+
+    this.userData = this.userService.GetUserFromLocalStorage();
+    if (this.userData) {
+      this.IsLogin = true;
+      this.userData.role.indexOf('Admin') > -1
+        ? (this.userPath = '/admin')
+        : (this.userPath = '/user');
+    }
+    this.spinner.hide();
   }
-  uploadImage(file:any)
-  {
-    if(file.length==0)
-    return ;
-    let fileToUpload=<File>file[0];//
-    const formDate=new FormData();//object
-    formDate.append('file',fileToUpload,fileToUpload.name);
-    debugger
+  uploadImage(file: any) {
+    if (file.length == 0) return;
+    let fileToUpload = <File>file[0]; //
+    const formDate = new FormData(); //object
+    formDate.append('file', fileToUpload, fileToUpload.name);
+    debugger;
     this.home.uploadAttachment(formDate);
   }
 
-  CreateContactUs(){
-    console.log(this.CreateContact.value)
+  CreateContactUs() {
+    console.log(this.CreateContact.value);
     this.home.createContact(this.CreateContact.value);
-    debugger
+    debugger;
   }
 }
-
-// {
-//   "id": 1,
-//   "name": "Telegram",
-//   "logo": "Logo",
-//   "img": "Img",
-//   "email": "telegram@gmail.com",
-//   "phone": "07716482",
-//   "address": "Jordan-Irbid"
-// }
